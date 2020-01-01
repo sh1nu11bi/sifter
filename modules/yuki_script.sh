f14c3eeff1a164688ef982cf3ee00b06ae9f798e
s1l3nt78 <44367179+s1l3n7h0s7@users.noreply.github.com>
2020-01-01 23:41:06 +0200

    Add files via upload

---
+++ modules/yuki_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+ORNG='\033[0;33m'
+W='\033[1;37m'
+
+echo -e "${ORNG}Yuki-Chan${NC}"
+echo -e "${ORNG}***********${NC}"
+cat files/pingtest_pass.txt
+echo -e "${W}Please copy and paste in your target site${NC}"
+echo "================================================================================================="
+cd /root/yuki
+sleep 1
+sudo ./yuki.sh
+echo "================================================================================================="
+./modules/module_runner.sh
