e6046a20e198a7d97d6bb5584d7a371510f28f11
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-01-29 21:45:01 +0200

    Merge remote-tracking branch 'origin/master'

--- modules/harvester_script.sh
+++ modules/harvester_script.sh
@@ -7,8 +7,22 @@ YLW='\033[1;33m'
 multi_scan() {
     for name in `cat files/pingtest.pass`
         do
-            theHarvester -d $name -S 300 -s -e 8.8.8.8 -n -c -b all -f results_${name}.html
+            echo -e "${W}How long would you like theHarvester to run for?(seconds)${NC}"
+            read TIME
+            sleep 1
+            theHarvester -d $name -S ${TIME} -s -e 8.8.8.8 -n -c -b all -f results_${name}.html
         done
+}
+single_scan() {
+    echo -e "${YLW}"
+    cat files/pingtest.pass
+    echo -e "${NC}"
+    echo -e "${W}Please enter your target${NC}"
+    read TARGET
+    echo -e "${W}How long would you like theHarvester to run for?(seconds)${NC}"
+    read TIME
+    sleep 1
+    theHarvester -d ${TARGET} -S ${TIME} -s -e 8.8.8.8 -n -c -b all -f results_${name}.html
 }
 
 echo -e "${ORNG}theHarvester${NC}"
@@ -20,13 +34,7 @@ echo -e "${W}Would you like to use a single target or the whole list?(s/l)${NC}"
 read ANS
 sleep 1
 if [[ ${ANS} == s ]]; then
-    echo -e "${YLW}"
-    cat files/pingtest.pass
-    echo -e "${NC}"
-    echo -e "${W}Please enter your target${NC}"
-    read TARGET
-    sleep 1
-    theHarvester -d ${TARGET} -S 300 -s -e 8.8.8.8 -n -c -b all -f results_${name}.html
+    single_scan
 else
     multi_scan
 fi
