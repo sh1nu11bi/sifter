3b0898045320b595234372ee38b1329c76593a4f
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-03-25 09:15:21 +0200

    Update asnip.sh

--- modules/netmods/asnip.sh
+++ modules/netmods/asnip.sh
@@ -7,11 +7,18 @@ YLW='\033[1;33m'
 LBBLUE='\e[104m'
 RED='\033[0;31m'
 
+if [[ -d "/opt/sifter/results/aSnip" ]]; then
+	sleep 1
+else 
+	mkdir /opt/sifter/results/asnip
+fi
 echo -e "${ORNG}"
 figlet -f mini "aSnip"
 echo -e "${NC}"
 echo -e "${W}Please enter your target${NC}"
 read TARGET
+mkdir /opt/sifter/results/aSnip/${TARGET}
+cd /opt/sifter/results/aSnip/${TARGET}
 asnip -t ${TARGET} -p
 sleep 2
 echo "===================================================================="
