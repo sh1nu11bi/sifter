d630a0e0631074c432be39f31b80929d4eb34545
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-07-18 02:55:10 +0200

    Quicker Updates

--- modules/vulnscanners/yuki.sh
+++ modules/vulnscanners/yuki.sh
@@ -5,17 +5,24 @@ ORNG='\033[0;33m'
 W='\033[1;37m'
 YLW='\033[1;33m'
 LBBLUE='\e[104m'
+FIRST='YES'
 
-echo -e "${RED}Doing final Perl configurations${NC}"
 cd /root/yuki/
-sudo bash install-perl-module.sh
+if [[ ${FIRST} == 'YES' ]]; then
+    REP="FIRST='YES'"
+    RER="FULL='NO'"
+    echo -e "${RED}Doing final Perl configurations${NC}"
+    sudo bash install-perl-module.sh
+    sed -i "s/${REP}/${RER}/g" /opt/sifter/modules/vulnscanners/yuki.sh
+fi
+
 echo -e "${ORNG}"
 figlet -f small "Yuki-Chan"
 echo -e "${NC}"
 echo -e "${YLW}"
 cat files/pingtest.pass
 echo -e "${NC}"
-echo -e "${W}Please copy and paste in your target site${NC}"
+echo -e "${W}Please copy and paste in your target site\nOr enter as per example above${NC}"
 echo "================================================================================================="
 cd /root/yuki
 sleep 1
