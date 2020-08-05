ca686b2e9868464e050ab2e13bab961a1488b393
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-08-06 01:10:09 +0200

    9.2 (Final Adj)

--- install.sh
+++ install.sh
@@ -1566,6 +1566,6 @@ echo ""
 ######################               VGhlIERlYWQgQnVubnkgQ2x1Yg==               ######################
 ######################################################################################################
 echo -e "${YLW}#################"                                                    ########################
-echo -e "${ORNG}# Version :${LP} 9.1 ${ORNG}#${NC}"                                  ##    VERSION INFO    ##
+echo -e "${ORNG}# Version :${LP} 9.2 ${ORNG}#${NC}"                                  ##    VERSION INFO    ##
 #echo -e "${ORNG}# Revision: ${LP}2  ${ORNG}#"                                        ##    UPDATE CHECK    ##
 echo -e "${YLW}#################${NC}"                                               ########################
