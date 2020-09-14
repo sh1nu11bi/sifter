9a64a52c3823c534d610f26e1c42c101af23a89a
s1l3nt78 <sl3n7h0s7@gmail.com>
2020-09-14 12:25:30 +0200

    EoP & unzip

--- modules/postex/EoP.sh
+++ modules/postex/EoP.sh
@@ -9,10 +9,17 @@ UBLUE='\033[4;34m'
 
 cd /opt/CVE-2020-0683
 echo -e "${INV}"
+sudo cp EoP.zip -t /home/$USER/Desktop
+sudo cp /opt/sifter/extras/unzip.exe -t /home/$USER/Desktop
+sudo chown $USER:$USER /home/$USER/Desktop/EoP.zip /home/$USER/Desktop/unzipme.exe
 figlet -f mini "EoP Exploit (CVE-2020-0683)"
 echo -e "${URED}EoP.zip ${W}has been copied to your Desktop. Please transfer this file to target and unzip in a CLI"
 echo -e "then cd into bin_MsiExploit. Please ensure MsiExploit.exe & foo.msi stay in the same directory or this will fail!"
 echo -e "then run ${UBLUE}"MsiExploit.exe  \path\file\to\pwn" ${W}from a command terminal on target${NC}"
+echo -e "     ${W}===================================== ${NC} "
+echo -e "${ORNG}unzip.exe ${INV} has been moved to your desktop as well \nso you can easily unzip your files${NC}"
+
+sleep 10
 
 
 ##########################______________ VGhlIERlYWQgQnVubnkgQ2x1Yg== ______________##########################
