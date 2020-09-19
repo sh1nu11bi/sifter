c402b0d19ddac25579f364826b76f1a857c5e5fd
s1l3nt78 <sl3n7h0s7@gmail.com>
2020-09-19 02:20:31 +0200

    Update ban2.py

--- extras/ban2.py
+++ extras/ban2.py
@@ -3,7 +3,8 @@ CVIOLET = '\33[35m'
 CGREENBG2  = '\33[104m'
 CRED = '\033[91m'
 CBLUE = '\33[94m'
-CYELLOW = '\33[33m'
+ORANGE = '\33[33m'
+CYAN = '\033[01m\33[31m'
 CEND = '\033[0m'
 UGREEN = '\33[92m'
 WHITE = '\33[37m'
@@ -14,6 +15,6 @@ def showbanner():
   ______  /  ___/|   |  |   _(__  < /    \   __\  /    />      <   ______
  /_____/  \___ \ |   |  |__/       \   |  \  |   /    //   --   \ /_____/
          /____  >|___|____/______  /___|  /__|  /____/ \______  /
-              \/       ''' + WHITE + "Gemin1" + CEND + " " + CVIOLET + '''    \/    \/     ''' + UGREEN + "Version" + CBLUE + " 10" + CEND + CVIOLET + '''    \/    ''' + CEND)
-        print(WHITE + "==============================   " + CYELLOW + "Revision " + CBLUE + "2" + WHITE + "   ==============================    " + CEND)
+              \/       ''' + CYAN + "G" + WHITE + "em" + CYAN + "i" + WHITE + "n" + CYAN + "1" + CVIOLET + '''    \/     \/     ''' + UGREEN + "Version" + CBLUE + " 10" + CVIOLET + '''     \/    ''' + CEND)
+        print(WHITE + "==============================   " + ORANGE + "Revision " + CBLUE + "2" + WHITE + "   ==============================    " + CEND)
 showbanner()
