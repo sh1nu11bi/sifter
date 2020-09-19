2a4225b47c594b7c54e667e67dcc05b53fc37cbf
s1l3nt78 <sl3n7h0s7@gmail.com>
2020-09-19 02:43:57 +0200

    r2

--- install.sh
+++ install.sh
@@ -1854,6 +1854,6 @@ echo -e "${RED}=================================================================
 ######################               VGhlIERlYWQgQnVubnkgQ2x1Yg==             ########################
 ######################################################################################################
 echo -e "${YLW}#########################"                                             ########################
-echo -e "${ORNG}# Incoming Version :${LP} 10 ${ORNG}#${NC}"                          ##    VERSION INFO    ##
-echo -e "${ORNG}#     Revision: ${LP}2       ${ORNG}#"                                       ##    UPDATE CHECK    ##
+echo -e "${YLW}# ${ORNG}Incoming Version :${LP} 10 ${YLW}#${NC}"                          ##    VERSION INFO    ##
+echo -e "${YLW}#          ${ORNG}Revision: ${LP}2 ${YLW}#"                                       ##    UPDATE CHECK    ##
 echo -e "${YLW}#########################${NC}"                                         ########################
