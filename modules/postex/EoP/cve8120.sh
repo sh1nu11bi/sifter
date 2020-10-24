3bd93d243931dc6774179c0dd1d537d64dc938bc
wh7rb7 <44367179+s1l3nt78@users.noreply.github.com>
2020-10-24 15:50:57 +0200

    Update cve8120.sh

--- modules/postex/EoP/cve8120.sh
+++ modules/postex/EoP/cve8120.sh
@@ -12,7 +12,8 @@ cp /opt/sifter/modules/postex/EoP/cve-2018-8120.zip -t /home/$USER/Desktop
 cp /opt/sifter/extras/unzip.exe -t /home/$USER/Desktop
 sudo chown $USER:$USER /home/$USER/Desktop/cve-2018-8120.zip /home/$USER/Desktop/unzip.exe
 figlet -f mini "CVE-2018-8120"
-echo -e "${URED}cve-2018-8120.zip ${W}has been copied to your Desktop. Please transfer this file to target and unzip in a CLI"
+echo -e "${URED}cve-2018-8120.zip ${W}has been copied to your Desktop. (Currently only Release binary provided)\nIf you would like to compile the binaries yourself\n${YLW}Please clone https://github.com/unamer/CVE-2018-8120 & put the build binaries in ${ORNG}cve-2018-8120.zip${YLW} in ${W}/opt/sifter/modules/postex/EoP${NC}"
+echo -e "${W}Please transfer this file to target and unzip in a CLI"
 echo -e "then run ${UBLUE}'cve-2018-8120.exe <command>/<file>' ${W}from a command terminal on target${NC}"
 echo -e "     ${W}===================================== ${NC} "
 echo -e "${ORNG}unzip.exe ${INV} has been moved to your desktop as well \nso you can easily unzip your files${NC}"
