ef4679fb700e434f6635fe659c273f55ec4b0453
Whi73Rabb17 <s1l3n7h0s7@gmail.com>
2020-11-13 18:25:48 +0200

    Webmap fix

--- modules/netmods/webmap.sh
+++ modules/netmods/webmap.sh
@@ -16,7 +16,27 @@ URED='\033[4;31m'
 echo -e "${ORNG}"
 figlet -f mini "WebMap"
 echo -e "${NC}"
-sudo docker start webmap
+sudo docker run -d \
+			--name webmap \
+			-h webmap \
+			-p 8000:8000 \
+			-v /tmp/webmap:/opt/xml \
+			reborntc/webmap
+if [[ ! -f '/opt/WebMap/.token' ]]; then
+		sudo docker start webmap
+		TKN=$(sudo docker exec -ti webmap /root/token) 
+		sudo touch /opt/WebMap/.token
+		sudo chown $USER:$USER /opt/WebMap/.token
+		echo ${TKN} > /opt/WebMap/.token
+fi
+if [[ ! -f '/opt/WebMap/.browser' ]]; then
+		sudo docker start webmap
+		sudo touch /opt/WebMap/.browser
+		sudo chown $USER:$USER /opt/WebMap/.browser		
+		echo -e "${YLW}For runtime the WebMap module will attempt to automatically open the local browser to access the panel\nAs not everyone uses the same browser and to make this as compatible as possible while not installing to many extras.\n${W}Please enter the termianl command for your browser. (eg. ${ORNG}chromium ${W}| ${ORNG}firefox ${W}| ${ORNG}firefox-esr ${W}| ${ORNG}chrome)${NC}"
+		read BROWSE
+		echo ${BROWSE} > /opt/WebMap/.browser
+fi
 echo -e "${YLW}Please enter the target IP or block: ${NC}"
 read TAR
 TKN=$(cat /opt/WebMap/.token)
@@ -29,6 +49,7 @@ echo -e "${RED}NOTE: ${YLW}Please enter ${URED}stop${NC} ${YLW}when you are done
 read DONE
 if [[ ${DONE} == "stop" ]]; then
 	sudo docker stop webmap
+	sudo docker container rm webmap --force
 fi
 
 ##########################______________ VGhlIERlYWQgQnVubnkgQ2x1Yg== ______________##########################
